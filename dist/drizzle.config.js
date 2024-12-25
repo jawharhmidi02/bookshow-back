4b7ea34ba13ce3db353d88a40339de1f39ae704e
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-12-25 16:28:18 +0100

    .

--- dist/drizzle.config.js
+++ dist/drizzle.config.js
@@ -1,14 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const drizzle_kit_1 = require("drizzle-kit");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const dotenv_1 = require('dotenv');
+const drizzle_kit_1 = require('drizzle-kit');
 (0, dotenv_1.config)({ path: '.env' });
 exports.default = (0, drizzle_kit_1.defineConfig)({
-    schema: './src/schema/*.ts',
-    out: './supabase/migrations',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL,
-    },
+  schema: './src/schema/*.ts',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
 });
 //# sourceMappingURL=drizzle.config.js.map
---
+++ dist/main.js
@@ -0,0 +1,13 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const core_1 = require('@nestjs/core');
+const app_module_1 = require('./app.module');
+async function bootstrap() {
+  const app = await core_1.NestFactory.create(app_module_1.AppModule);
+  app.enableCors();
+  const PORT = process.env.PORT || 5001;
+  console.log('Server is listening on ', PORT);
+  await app.listen(PORT);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
